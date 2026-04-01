284b32dd90744ee5635c5c66cb2d8735d395c482
k1mjunyoung <me@kimjunyoung.com>
2026-04-01 15:53:06 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl">Openbird</h1>
+    </div>
+  );
+}
